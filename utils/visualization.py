84d72805d4a2079a9dcb2e659a02bf7a6e5d1ac4
kwea123 <kwea123@gmail.com>
2020-03-06 10:16:41 +0900

    add nan to zero

--- utils/visualization.py
+++ utils/visualization.py
@@ -9,8 +9,10 @@ def visualize_depth(depth, cmap=cv2.COLORMAP_JET):
     depth: (H, W)
     """
     x = depth.cpu().numpy()
-    mi = x[x>0].min() # get minimum positive depth (ignore background)
-    x = (x-mi)/(x.max()-mi) # normalize to 0~1
+    x = np.nan_to_num(x) # change nan to 0
+    mi = np.min(x[x>0]) # get minimum positive depth (ignore background)
+    ma = np.max(x)
+    x = (x-mi)/(ma-mi+1e-8) # normalize to 0~1
     x = (255*x).astype(np.uint8)
     x_ = Image.fromarray(cv2.applyColorMap(x, cmap))
     x_ = T.ToTensor()(x_) # (3, H, W)
